158cd23bc8a0a491422d797ca7466aa11cc24037
Ozgen <ozgen.akgun.dikili@gmail.com>
2023-02-03 10:13:37 -0800

    Added New Company Role Hourly rate page
    Missing Map - First need to try on a hello world project

---
+++ app/src/main/java/com/example/punchmytimebgv01/NewCompanyRoleRate.java
@@ -0,0 +1,14 @@
+package com.example.punchmytimebgv01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCompanyRoleRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_company_role_rate);
+    }
+}
